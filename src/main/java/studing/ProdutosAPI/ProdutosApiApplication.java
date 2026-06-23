a93c0f82d7f14a53f2f25a52da6cf7e967a2da7a
Pedro Lucas <pedrolbcubinhese@gmail.com>
2026-06-23 19:12:18 -0300

    config project

--- src/main/java/studing/ProdutosAPI/ProdutosApiApplication.java
+++ src/main/java/studing/ProdutosAPI/ProdutosApiApplication.java
@@ -1,4 +1,4 @@
-package io.github.cursodsousa.produtosapi;
+package studing.ProdutosAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
